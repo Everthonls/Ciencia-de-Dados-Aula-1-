692d9222b513e6b03237d9babfd5eb67216ef7e5
Everthonls <119797342+Everthonls@users.noreply.github.com>
2022-12-05 22:47:18 +0000

    numeros pares

---
+++ Exercicio 01.py
@@ -0,0 +1,18 @@
+total = 0
+num1 = int(input("Digite o primeiro valor:"))
+if num1 % 2 == 0:
+    total = total + 1
+num2 = int(input("Digite o segundo valor:"))
+if num2 % 2 == 0:
+    total = total + 1
+num3 = int(input("Digite o terceiro valor:"))
+if num3 % 2 == 0:
+    total = total + 1
+num4 = int(input("Digite o quarto valor:"))
+if num4 % 2 == 0:
+    total = total + 1
+num5 = int(input("Digite o quinto valor:"))
+if num5 % 2 == 0:
+    total = total + 1
+
+print("Essa é a quantidade de números pares:",total)
