ec35313fbe5396c3bee824d7ec1e491780d69967
Everthonls <119797342+Everthonls@users.noreply.github.com>
2022-12-07 00:26:29 +0000

    Senha Incorreta

---
+++ Exercicio 03.py
@@ -0,0 +1,8 @@
+while True:
+    i = int(input("Digite sua senha de quatro dígitos:"))
+
+    if i != 2002:
+        print("Senha incorreta!")
+    else:
+        print ("Acesso liberado!" )
+        break
